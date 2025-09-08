3b356e8ea10c8b8a9039175a95df527092e4ca81
jahmes <102207795+jahmeswh@users.noreply.github.com>
2025-09-08 10:39:35 +0800

    Add files via upload

---
+++ Sample1.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "James Timcang Bracamonte";
+        int age = 18;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
