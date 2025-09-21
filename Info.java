a0e56097d5ad59c769df95723e5c6f01d8011f48
jahmes <102207795+jahmeswh@users.noreply.github.com>
2025-09-22 07:01:06 +0800

    Add files via upload

---
+++ Info.java
@@ -0,0 +1,18 @@
+public class Info {
+	public static void main(String[] args) {
+	
+        String name = "James T. Bracamonte";
+        int age = 18;
+        String address = "Purok 8, Brgy. Tinambacan Sur, Calbayog City";
+        String birthday = "December 25, 2006";
+        String program = "Bachelor Of Science Major In Computer Science";
+        String university = "Northwest Samar State University";
+
+        System.out.println("My name is " + name); 
+        System.out.println("I am " +  age  + " years old");
+        System.out.println("I was born on " + birthday);
+        System.out.println("I live in " + address);
+        System.out.println("I am currently enrolled in " + university);
+        System.out.println("Pursuing " + program);
+	}
+}
