e19f2901b7fd15677990a7fda757a1643c86a1df
jahmes <102207795+jahmeswh@users.noreply.github.com>
2025-09-25 10:56:49 +0800

    Delete Sample1.java

--- Sample1.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String name = "James Timcang Bracamonte";
-        int age = 18;
-
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-    }
-}
